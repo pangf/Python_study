434fa1f30d3c85b2bbc0a2d65c7cce4af9fccad2
miru <miru11104627@gmail.com>
2019-07-16 00:23:53 -0400

    version 0.1

---
+++ name.py
@@ -0,0 +1,10 @@
+#use input() to get massage for user
+#use print() to print it out
+
+name=input("pls input your name:")
+print("+++++++++++++++++++++++++")
+address=input("pls input your address:")
+print("+++++++++++++++++++++++++")
+print("what is your name:%s"%name)
+print("+++++++++++++++++++++++++")
+print("Adress is:%s"%address)
